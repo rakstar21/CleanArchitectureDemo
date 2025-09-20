cbb3f9bb6418a53f36da7aada868df5aaff58954
rakstar21 <daddyrakstar@gmail.com>
2025-09-20 10:42:30 +0800

    Add domain validations

---
+++ CleanCodeArchitectureDemo.Domain/Modelling/Validation/ValidationResult.cs
@@ -0,0 +1,16 @@
+﻿using CleanCodeArchitectureDemo.Domain.Modelling.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCodeArchitectureDemo.Domain.Modelling.Validation
+{
+    public class ValidationResult<T> where T : IDomain
+    {
+        public bool IsValid { get; set; }
+        public IEnumerable<ValidationError<T>> ValidationErrors { get; set; } = new List<ValidationError<T>>();
+    }
+}
